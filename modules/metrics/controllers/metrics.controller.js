3d404996727a7220e4a2b5625bf90fcb187a1df1
lemanahuel <lema.nahuel@gmail.com>
2016-12-08 12:33:34 -0300

    IMPROVE, metrics

--- modules/metrics/controllers/metrics.controller.js
+++ modules/metrics/controllers/metrics.controller.js
@@ -6,24 +6,41 @@ angular
     function(Incidents, Users, $state, $scope) {
       var vm = this;
       var incidentes = Incidents && Incidents.data;
+      var users = Users && Users.data;
+      var comunas = [];
 
       function normalizeComuna(comuna) {
         return comuna && comuna.toLowerCase().replace(' ', '-');
       }
 
+      function normalizeComunas() {
+        incidentes = incidentes.map(function(item) {
+          item.comuna = normalizeComuna(item.comuna);
+          return item;
+        });
+      };
+
+      normalizeComunas();
+
       function setComunas() {
-        var amountOfIncidentsByComuna = [];
-        var amountOfResponseByComuna = [];
-        var comunas = incidentes.map(function(item) {
-          return normalizeComuna(item.comuna);
+        comunas = incidentes.map(function(item) {
+          return item.comuna;
         }).filter(function(item) {
           return !!item;
         }).filter(function(item, i, ar) {
           return ar.indexOf(item) === i;
         });
+      }
+
+      setComunas();
+
+      function setIncidentsChart() {
+        var amountOfIncidentsByComuna = [];
+        var amountOfResponseByComuna = [];
+
         comunas.forEach(function(comuna) {
           var currents = incidentes.filter(function(incident) {
-            return normalizeComuna(incident.comuna) === comuna;
+            return incident.comuna && incident.comuna === comuna;
           });
           var responses = currents.map(function(item) {
             return item.responses.length;
@@ -47,12 +64,55 @@ angular
         vm.incidentsTable = incidentsTable;
       }
 
-      setComunas();
+      setIncidentsChart();
 
-      // Incidentes(cantidad de incidentes por comuna.Promedio de respuestas.)
+      function setVolunteersChart() {
+        var volunteersByComuna = 0;
+        var civils = users.filter(function(item) {
+          return !item.isVolunteer;
+        });
+        var volunteers = users.filter(function(item) {
+          return item.isVolunteer;
+        });
+
+        vm.volunteersLabels = ['Civil', 'Voluntarios'];
+        vm.volunteersData = [civils.length, volunteers.length];
+
+        var volunteersTable = [];
+        var amountOfVolunteersByComuna = [];
+        var amountOfCivilsByComuna = [];
+        var comunas = volunteers.map(function(item) {
+          return normalizeComuna(item.comuna);
+        }).filter(function(item) {
+          return !!item;
+        }).filter(function(item, i, ar) {
+          return ar.indexOf(item) === i;
+        });
+
+        comunas.forEach(function(comuna) {
+          amountOfVolunteersByComuna.push(volunteers.filter(function(item) {
+            return item.comuna && normalizeComuna(item.comuna) === comuna;
+          }).length);
+          amountOfCivilsByComuna.push(civils.filter(function(item) {
+            return item.comuna && normalizeComuna(item.comuna) === comuna;
+          }).length);
+        });
+
+        comunas.forEach(function(comuna, i) {
+          volunteersTable.push({
+            comuna: comuna,
+            volunteers: amountOfVolunteersByComuna[i],
+            civils: amountOfCivilsByComuna[i]
+          });
+        });
+
+        vm.volunteersByComunaData = amountOfVolunteersByComuna;
+        vm.volunteersByComunaLabels = comunas;
+        vm.volunteersTable = volunteersTable;
+      }
+
+      setVolunteersChart();
+
       // Voluntarios(Cuantos voluntarios por comuna, y cuantos usuarios son voluntarios, masculinos y femeninos)
-
-      $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-      $scope.data = [30, 50, 10];
     }
   ]);
